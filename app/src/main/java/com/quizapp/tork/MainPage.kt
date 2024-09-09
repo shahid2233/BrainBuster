c458c220e827444a15754f2e3108181d47465c80
Deepconjerour <abusufiyan.shaikh09@gmail.com>
2024-09-09 15:17:41 +0530

    Added.

--- app/src/main/java/com/quizapp/tork/MainPage.kt
+++ app/src/main/java/com/quizapp/tork/MainPage.kt
@@ -1,4 +1,25 @@
 package com.quizapp.tork
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+//import kotlinx.android.synthetic.main_page.*
 class MainPage {
+    class MainPage : AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.main_page)
+
+            val myButton: Button = findViewById(R.id.button)
+            myButton.setOnClickListener {
+                // Handle button click here
+                val intent = Intent(this, LoginScreen::class.java)
+                startActivity(intent)
+            }
+        }
+    }
 }
